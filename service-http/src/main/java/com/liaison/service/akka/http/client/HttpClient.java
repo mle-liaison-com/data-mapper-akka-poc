19897e9d3a1d3c2d1a9ea95f42efa630acb844c2
hpark-liaison-com <hpark@liaison.com>
2017-12-12 16:29:12 +0900

    let user provide circuit breaker function as well

--- service-http/src/main/java/com/liaison/service/akka/http/client/HttpClient.java
+++ service-http/src/main/java/com/liaison/service/akka/http/client/HttpClient.java
@@ -5,41 +5,28 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.pattern.CircuitBreaker;
-import com.liaison.service.akka.core.circuitbreaker.CircuitBreakerBuilder;
-import com.liaison.service.akka.core.circuitbreaker.CircuitBreakerFactory;
-import com.typesafe.config.Config;
-import scala.concurrent.duration.FiniteDuration;
 
 import javax.annotation.Nonnull;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public class HttpClient {
 
-    private static final String CONFIG_HTTP_MAX_FAILURE = "com.liaison.service.akka.http.client.failure.max";
-    private static final String CONFIG_HTTP_TIMEOUT_CALL = "com.liaison.service.akka.http.client.failure.timeout.call";
-    private static final String CONFIG_HTTP_TIMEOUT_RESET = "com.liaison.service.akka.http.client.failure.timeout.reset";
-
     private final ActorSystem system;
-    private final BiConsumer<HttpResponse, Throwable> action;
+    private final Function<HttpRequest, CircuitBreaker> circuitBreakerFunction;
+    private final BiConsumer<HttpResponse, Throwable> responseHandler;
 
-    public HttpClient(@Nonnull ActorSystem system, @Nonnull BiConsumer<HttpResponse, Throwable> action) {
+    public HttpClient(@Nonnull ActorSystem system,
+                      @Nonnull Function<HttpRequest, CircuitBreaker> circuitBreakerFunction,
+                      @Nonnull BiConsumer<HttpResponse, Throwable> responseHandler) {
         this.system = system;
-        this.action = action;
+        this.circuitBreakerFunction = circuitBreakerFunction;
+        this.responseHandler = responseHandler;
     }
 
     public void call(HttpRequest request) {
-        CircuitBreaker circuitBreaker = CircuitBreakerFactory.getInstance(request.getUri().getHost().address(), str -> {
-            Config config = system.settings().config();
-            CircuitBreakerBuilder builder = new CircuitBreakerBuilder(system.dispatcher(),
-                    system.scheduler(),
-                    config.getInt(CONFIG_HTTP_MAX_FAILURE),
-                    FiniteDuration.apply(config.getLong(CONFIG_HTTP_TIMEOUT_CALL), TimeUnit.MILLISECONDS),
-                    FiniteDuration.apply(config.getLong(CONFIG_HTTP_TIMEOUT_RESET), TimeUnit.MILLISECONDS));
-            return builder.build();
-        });
-
+        CircuitBreaker circuitBreaker = circuitBreakerFunction.apply(request);
         Http http = Http.get(system);
-        circuitBreaker.callWithCircuitBreakerCS(() -> http.singleRequest(request)).whenComplete(action);
+        circuitBreaker.callWithCircuitBreakerCS(() -> http.singleRequest(request)).whenComplete(responseHandler);
     }
 }
